53a42772f7a65bc5f3c7aeb07c9ac5b48931ce86
kudik_lap <kudik233@gmail.com>
2022-10-12 11:14:41 +0200

    moja wersja

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ class input():
         self.maximum = maxi
         self.sections = {}
 
+
     def add_section(self,nodes:list,name:str):
         if len(nodes) == 3:
             self.sections[name] = nodes
@@ -35,17 +36,18 @@ class input():
         plt.show()
 
     def __and__(self,other):
-        print(np.interp(6,self.sections['poor1'],y2))
+        
         return # dwie
 
-    def __or__(self, other):
+    # def __or__(self, other):
 
-
+#print(np.interp(6,self.sections['poor1'],y2))
 
 input1 = input(0,10)
 input1.add_section([0,1,2,3],'poor')
 input1.add_section([2,3,4,5],'avg')
 input1.add_section([4,6,8,10],'good')
+print(type(input1))
 #print(input1.sections)
 input1.draw_input()
 
